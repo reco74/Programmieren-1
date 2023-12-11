9048849a5364902822dcc099646fca4f18ade533
reco74 <152899081+reco74@users.noreply.github.com>
2023-12-11 21:02:43 +0100

    Update SpieleNachAlter.py

--- SpieleNachAlter.py
+++ SpieleNachAlter.py
@@ -13,47 +13,47 @@ spiele18 = ['GTA 5', 'Red Dead Redemption', 'God of War']
 
 alter = int(input("Wie alt bist du?: "))
 
-    if alter < 6:
-        print("Spiele unter 6 Jahren: \n")
-        for i in spiele0:
-            print(i)
+if alter < 6:
+    print("Spiele unter 6 Jahren: \n")
+    for i in spiele0:
+        print(i)
 
-    elif alter < 12:
-        print("Spiele unter 12 Jahren: \n")
-        for i in spiele0:
-            print(i)
-        for b in spiele6:
-            print(b)
+elif alter < 12:
+    print("Spiele unter 12 Jahren: \n")
+    for i in spiele0:
+        print(i)
+    for b in spiele6:
+        print(b)
 
-    elif alter < 16:
-        print("Spiele unter 16 Jahren: \n")
-        for i in spiele0:
-            print(i)
-        for b in spiele6:
-            print(b)
-        for a in spiele12:
-            print(a)
+elif alter < 16:
+    print("Spiele unter 16 Jahren: \n")
+    for i in spiele0:
+        print(i)
+    for b in spiele6:
+        print(b)
+    for a in spiele12:
+        print(a)
 
-    elif alter < 18:
-        print("Spiele unter 18 Jahren: \n")
-        for i in spiele0:
-            print(i)
-        for b in spiele6:
-            print(b)
-        for a in spiele12:
-            print(a)
-        for n in spiele16:
-            print(n)
+elif alter < 18:
+    print("Spiele unter 18 Jahren: \n")
+    for i in spiele0:
+        print(i)
+    for b in spiele6:
+        print(b)
+    for a in spiele12:
+        print(a)
+    for n in spiele16:
+        print(n)
 
-    elif alter >= 18:
-        print("Spiele unter und ab 18 Jahren: \n")
-        for i in spiele0:
-            print(i)
-        for b in spiele6:
-            print(b)
-        for a in spiele12:
-            print(a)
-        for n in spiele16:
-            print(n)
-        for k in spiele18:
-            print(k)
+elif alter >= 18:
+    print("Spiele unter und ab 18 Jahren: \n")
+    for i in spiele0:
+        print(i)
+    for b in spiele6:
+        print(b)
+    for a in spiele12:
+        print(a)
+    for n in spiele16:
+        print(n)
+    for k in spiele18:
+        print(k)
